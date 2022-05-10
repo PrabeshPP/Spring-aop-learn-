7a3279e0884de51e2d63aa77242087d7750191ba
Prabesh <prabesh.bista369@gmail.com>
2022-05-10 10:18:58 +0545

    Aspect Oriented Programming

--- src/main/java/org/springaop/AuthenticationAspect.java
+++ src/main/java/org/springaop/AuthenticationAspect.java
@@ -1,4 +1,25 @@
 package org.springaop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class AuthenticationAspect {
+    @Pointcut("within(org.springaop..*)")
+    public void authenticationPointCut(){
+        System.out.println("Authenticated");
+    }
+
+    @Pointcut("within(org.springaop.*)")
+    public void authorizationPointCut(){
+        System.out.println("Authorized");
+    }
+
+    @Before("authenticationPointCut() && authorizationPointCut()")
+    public void authenticate(){
+        System.out.println("Authenticating the Request.....");
+    }
 }
